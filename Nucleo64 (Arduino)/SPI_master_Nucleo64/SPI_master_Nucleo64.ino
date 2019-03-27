20e1ff8b65e615b2bd33e7659e82d13589270fc1
Albert Sandager <albert@sandbo.dk>
2019-03-27 08:25:50 +0100

    nucleo code :3

---
+++ Nucleo64 (Arduino)/SPI_master_Nucleo64/SPI_master_Nucleo64.ino
@@ -0,0 +1,27 @@
+#include "SPI.h" // necessary library
+int ss = 10; // using digital pin 10 for SPI slave select
+
+
+void setup() {
+  
+  pinMode(ss, OUTPUT); // we use this for SS pin
+  SPI.begin(); // wake up the SPI bus.
+  SPI.setBitOrder(MSBFIRST); //MSB (most significant byte) first
+  pinMode(3, INPUT_PULLUP);
+  SPI.setClockDivider(SPI_CLOCK_DIV8);
+
+}
+
+void loop() {
+
+  digitalWrite(ss, LOW);
+
+  SPI.transfer(0); // send command byte
+
+  SPI.transfer(133); // send value (0~255)
+
+  digitalWrite(ss, HIGH);
+
+  delay(1000);
+
+}

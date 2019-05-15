d1578a4cff63f8f69e19eed1298959bd726ac7e4
Albert Sandager <albert@sandbo.dk>
2019-05-15 09:19:50 +0200

    ting

---
+++ EKG-SPI_ekg-SPI_slave-ARM/kode på ARM/TB_ARM/TB_ARM.ino
@@ -0,0 +1,27 @@
+#include "SPI.h"
+#include "ARM.h"
+
+ARM arm;
+
+byte kanal1 = 0x00;
+byte kanal2 = 0x10;
+byte kanal3 = 0x20;
+byte kanal4 = 0x30;
+byte kanal5 = 0x40;
+
+void setup() {
+  // put your setup code here, to run once:
+
+  arm.konfig();
+
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if (digitalRead(arm.trdy) == HIGH) {
+    
+    arm.Test_getData(kanal1);
+    
+  }
+}
